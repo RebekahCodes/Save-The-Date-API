69afefb02d516ac446525ea790e6fb3beb724621
Rebekah <rebekahcodes@gmail.com>
2024-04-25 21:26:55 +0100

    created POST request

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-// Import the pg (node-postgres) library
-import pg from "pg";
+//import dotenv so I can securly store the connection string in the .env file and reference it below.
+import dotenv from "dotenv";
+dotenv.config();
 
 // Retrieve the database connection string from environment variables
-const connectionString =
-  "postgres://rebekahanderson@localhost:5432/rebekahanderson";
+const connectionString = process.env.DB_CONNECTION_STRING;
+console.log("DB_CONNECTION_STRING:", connectionString);
 
 // Check if the connection string is not defined, and if so, throw an error
 if (!connectionString) {
-  throw new Error(
-    "No DB_CONNECTION_STRING defined. Did you load in your env variables?"
-  );
+  throw new Error("No DB_CONNECTION_STRING defined.");
 }
+
+// Import the pg (node-postgres) library
+import pg from "pg";
 
 // Export a new instance of pg.Pool, which will be used to interact with the PostgreSQL database
 export const pool = new pg.Pool({
