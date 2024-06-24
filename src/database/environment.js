652687ea5768e0397ab94476ce7023147eaf0e46
Rebekah Anderson <rebekahanderson@Rebekahs-MacBook-Air.local>
2024-06-24 16:44:38 +0100

    updated environment variables

--- src/database/environment.js
+++ src/database/environment.js
@@ -2,12 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Retrieve the database connection string from environment variables
-const connectionString = process.env.DB_CONNECTION_STRING;
-console.log("DB_CONNECTION_STRING:", connectionString);
+const connectionString = process.env.DATABASE_URL;
+console.log("DATABASE_URL:", connectionString);
 
 // Check if the connection string is not defined, and if so, throw an error
 if (!connectionString) {
-  throw new Error("No DB_CONNECTION_STRING defined.");
+  throw new Error("No DATABASE_URL defined.");
 }
 
 export default connectionString;
