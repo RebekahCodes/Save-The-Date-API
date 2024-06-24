b3ad40a4eaf39e2af0bd4e47d89b045f4106c366
Rebekah Anderson <rebekahanderson@Rebekahs-MacBook-Air.local>
2024-06-24 17:19:01 +0100

    ssl

--- src/database/pool.js
+++ src/database/pool.js
@@ -1,12 +1,12 @@
 import pg from "pg";
 import connectionString from "./environment.js"; // Import the connection string. uncomment after tests
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0; // Disables SSL certificate verification (not recommended for production!) Delete after testing
+//process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0; // Disables SSL certificate verification (not recommended for production!) Delete after testing
 
 // Export a new instance of pg.Pool
 export const pool = new pg.Pool({
   // Pass the connection string to the pool
   connectionString,
-  ssl: true, //Try deleting once deployed
+  //ssl: true, //Try deleting once deployed
 });
 
 export default pool;
