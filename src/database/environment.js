597a58a3267a9d3b8ec94ae1d96d2ed13bb73e95
Rebekah Anderson <rebekahanderson@Rebekahs-MacBook-Air.local>
2024-06-24 17:05:32 +0100

    added env to package.json

--- src/database/environment.js
+++ src/database/environment.js
@@ -1,5 +1,13 @@
 import dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+
+// Determine environment
+const env = process.env.NODE_ENV || "development";
+const envPath = path.resolve(process.cwd(), `.env.${env}`);
+
+// Load environment variables from the appropriate .env file
+dotenv.config({ path: envPath });
+
 
 // Retrieve the database connection string from environment variables
 const connectionString = process.env.DATABASE_URL;
